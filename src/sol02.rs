55473ebd78cc3ccdca167242593e4be5974baca8
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-12-05 22:23:09 +0300

    Day 2, part 2

--- src/sol02.rs
+++ src/sol02.rs
@@ -1,15 +1,20 @@
 pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
     let mut x = 0;
-    let mut y = 0;
+    let mut y = 0;  // also aim
+    let mut y2 = 0;
     for line in input.split_terminator('\n') {
         let (cmd, arg) = line.split_once(' ').unwrap();
         let arg: i32 = arg.parse().unwrap();
         match cmd {
-            "forward" => x += arg,
+            "forward" => {
+                x += arg;
+                y2 += arg * y;
+            }
             "down" => y += arg,
             "up" => y -= arg,
             _ => panic!("{}", line),
         }
     }
     out((x * y).to_string());
+    out((x * y2).to_string());
 }
