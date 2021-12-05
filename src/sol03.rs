95e51d08077e628a169a528c9990fe5d4af0f7e1
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-12-05 23:03:32 +0300

    Day 3, part 2

--- src/sol03.rs
+++ src/sol03.rs
@@ -1,29 +1,71 @@
-use std::cmp::Ordering::{Less, Equal, Greater};
-
 pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
-    let mut cnt = 0;
-    let mut cnt1: Vec<i32> = vec![];
-    for line in input.split_terminator('\n') {
-        cnt += 1;
-        if cnt1.is_empty() {
-            cnt1.resize(line.len(), 0);
-        }
-        for (i, c) in line.chars().enumerate() {
-            if c == '1' {
-                cnt1[i] += 1;
-            }
+    let lines: Vec<&[u8]> = input.split_terminator('\n')
+        .map(|s| s.as_bytes())
+        .collect();
+    let n = lines[0].len();
+    for &line in &lines {
+        assert_eq!(line.len(), n);
+    }
+
+    let mut gamma = vec![];
+    let mut epsilon = vec![];
+    for i in 0..n {
+        let c = most_common(&lines, i);
+        gamma.push(c);
+        epsilon.push(flip(c));
+    }
+    out((parse(&gamma) * parse(&epsilon)).to_string());
+
+    let mut lines_oxy = lines.clone();
+    for i in 0..n {
+        let c = most_common(&lines_oxy, i);
+        lines_oxy.retain(|line| line[i] == c);
+        if lines_oxy.len() == 1 {
+            break;
         }
     }
-    let mut gamma = 0;
-    let mut epsilon = 0;
-    for c in cnt1 {
-        gamma *= 2;
-        epsilon *= 2;
-        match (2 * c).cmp(&cnt) {
-            Less => epsilon += 1,
-            Equal => panic!(),
-            Greater => gamma += 1,
+    assert_eq!(lines_oxy.len(), 1);
+
+    let mut lines_co2 = lines.clone();
+    for i in 0..n {
+        let c = flip(most_common(&lines_co2, i));
+        lines_co2.retain(|line| line[i] == c);
+        if lines_co2.len() == 1 {
+            break;
         }
     }
-    out((gamma * epsilon).to_string());
+    assert_eq!(lines_co2.len(), 1);
+
+    out((parse(lines_oxy[0]) * parse(lines_co2[0])).to_string());
+}
+
+fn most_common(lines: &[&[u8]], idx: usize) -> u8 {
+    let mut cnt1 = 0;
+    for &line in lines {
+        if line[idx] == b'1' {
+            cnt1 += 1;
+        }
+    }
+    if 2 * cnt1 >= lines.len() {
+        b'1'
+    } else {
+        b'0'
+    }
+}
+
+fn flip(c: u8) -> u8 {
+    match c {
+        b'0' => b'1',
+        b'1' => b'0',
+        _ => panic!()
+    }
+}
+
+fn parse(s: &[u8]) -> i32 {
+    let mut res = 0;
+    for c in s {
+        res *= 2;
+        res += (c - b'0') as i32;
+    }
+    res
 }
