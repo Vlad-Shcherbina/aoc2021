340dc65921556378b0b67d95feb8238179782247
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-12-05 22:10:24 +0300

    Day 1, part 2

--- src/sol01.rs
+++ src/sol01.rs
@@ -2,11 +2,17 @@ pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
     let depths: Vec<i32> = input.split_whitespace()
         .map(|s| s.parse().unwrap())
         .collect();
-    let mut cnt = 0;
-    for i in 1..depths.len() {
-        if depths[i - 1] < depths[i] {
-            cnt += 1;
+
+    for width in [1, 3] {
+        let mut cnt = 0;
+        let mut prev = i32::MAX;
+        for w in depths.windows(width) {
+            let s: i32 = w.iter().sum();
+            if s > prev {
+                cnt += 1;
+            }
+            prev = s;
         }
+        out(cnt.to_string());
     }
-    out(cnt.to_string());
 }
