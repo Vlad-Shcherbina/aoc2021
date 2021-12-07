51da4a87d3d7abed6f7d7495b04cfa6a44136409
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-12-08 02:54:51 +0300

    Day 6, part 2

--- src/sol06.rs
+++ src/sol06.rs
@@ -1,12 +1,14 @@
 pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
-    let mut counts = [0; 9];
+    let mut counts = [0i64; 9];
     for s in input.trim_end().split(',') {
         counts[s.parse::<usize>().unwrap()] += 1;
     }
-    for _ in 0..80 {
+    for day in 1..=256 {
         let z = counts[0];
         counts.rotate_left(1);
         counts[6] += z;
+        if day == 80 || day == 256 {
+            out(counts.iter().sum::<i64>().to_string());
+        }
     }
-    out(counts.iter().sum::<i32>().to_string());
 }
