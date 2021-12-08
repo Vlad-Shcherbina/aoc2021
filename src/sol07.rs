996651d6d63710baf7874641282e952818c61b93
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-12-08 03:14:59 +0300

    Day 7, part 2

--- src/sol07.rs
+++ src/sol07.rs
@@ -5,4 +5,13 @@ pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
     let i = xs.len() / 2;
     xs.select_nth_unstable(i);
     out(xs.iter().map(|&x| (x - xs[i]).abs()).sum::<i32>().to_string());
+
+    let mean = xs.iter().sum::<i32>() / xs.len() as i32;
+    let part2 = (mean - 5 .. mean + 5).map(|m|
+        xs.iter().map(|&x| {
+            let d = (x - m).abs();
+            d * (d + 1) / 2
+        }).sum::<i32>()
+    ).min().unwrap();
+    out(part2.to_string());
 }
