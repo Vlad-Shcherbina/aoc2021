15b3452cd5f8e167a9f2b17f1c14fb0a9ea7228d
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-12-13 02:42:36 +0300

    Day 9, part 2

--- src/sol09.rs
+++ src/sol09.rs
@@ -21,4 +21,34 @@ pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
         }
     }
     out(part1.to_string());
+
+    let mut visited: Vec<bool> = hmap.iter().map(|&h| h == b'9').collect();
+    let mut sizes = vec![];
+    let mut q = vec![];
+    for idx in 0 .. w * h {
+        if visited[idx] {
+            continue;
+        }
+        visited[idx] = true;
+        let mut size = 1;
+        q.push(idx);
+        while let Some(idx) = q.pop() {
+            for delta in [-1, 1, w as isize, -(w as isize)] {
+                let idx2 = (idx as isize + delta) as usize;
+                if !visited[idx2] {
+                    visited[idx2] = true;
+                    q.push(idx2);
+                    size += 1;
+                }
+            }
+        }
+        sizes.push(size);
+    }
+    let t = sizes.len() - 3 - 1;
+    let (_, _, last3) = sizes.select_nth_unstable(t);
+    let mut part2 = 1;
+    for &mut size in last3 {
+        part2 *= size;
+    }
+    out(part2.to_string());
 }
