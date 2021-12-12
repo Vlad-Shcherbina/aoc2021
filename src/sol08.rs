4d92df6f73f752903d5b0fc469bf07bef80640b3
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-12-13 01:43:30 +0300

    Day 8, part 2

--- src/sol08.rs
+++ src/sol08.rs
@@ -1,13 +1,35 @@
 pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
-    let mut cnt = 0;
+    let digits = "abcfeg cf acdeg acdfg bcdf abdfg abdefg acf abcdefg abcdfg";
+    let fingerprints: Vec<_> = digits.split(' ')
+        .map(|digit| fingerprint(digits, digit))
+        .collect();
+
+    let mut part1 = 0;
+    let mut part2 = 0;
     for line in input.split_terminator('\n') {
-        let (_left, right) = line.split_once(" | ").unwrap();
+        let (digits, right) = line.split_once(" | ").unwrap();
+        let mut n = 0;
         for s in right.split(' ') {
             let k = s.len();
             if k == 2 || k == 4 || k == 3 || k == 7 {
-                cnt += 1;
+                part1 += 1;
             }
+
+            n *= 10;
+            let f = fingerprint(digits, s);
+            n += fingerprints.iter().position(|f1| f1 == &f).unwrap();
         }
+        part2 += n;
     }
-    out(cnt.to_string());
+    out(part1.to_string());
+    out(part2.to_string());
+}
+
+fn fingerprint(digits: &str, s: &str) -> [u8; 8] {
+    let mut res = [0u8; 8];
+    for digit in digits.split(' ') {
+        let overlap = s.chars().filter(|&c| digit.contains(c)).count();
+        res[overlap] += 1;
+    }
+    res
 }
