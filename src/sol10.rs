c4e0aeef69647069b32406f7f011ef9ad150d90a
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-12-13 03:02:47 +0300

    Day 10, part 2

--- src/sol10.rs
+++ src/sol10.rs
@@ -1,20 +1,36 @@
 pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
     let mut part1 = 0;
+    let mut scores = vec![];
     for line in input.split_terminator('\n') {
         let mut stack = vec![];
+        let mut corrupted = false;
         for c in line.chars() {
             match closing(c) {
                 Some(c1) => stack.push(c1),
                 None => {
                     if c != stack.pop().unwrap() {
                         part1 += points(c);
+                        corrupted = true;
                         break;
                     }
                 }
             }
         }
+        if !corrupted {
+            let mut score = 0;
+            for &c in stack.iter().rev() {
+                score *= 5;
+                score += points2(c);
+            }
+            scores.push(score);
+        }
     }
     out(part1.to_string());
+
+    assert_eq!(scores.len() % 2, 1);
+    let t = scores.len() / 2;
+    let (_, middle, _) = scores.select_nth_unstable(t);
+    out(middle.to_string());
 }
 
 fn closing(c: char) -> Option<char> {
@@ -37,6 +53,16 @@ fn points(c: char) -> i32 {
         ']' => 57,
         '}' => 1197,
         '>' => 25137,
+        _ => panic!(),
+    }
+}
+
+fn points2(c: char) -> i64 {
+    match c {
+        ')' => 1,
+        ']' => 2,
+        '}' => 3,
+        '>' => 4,
         _ => panic!(),
     }
 }
