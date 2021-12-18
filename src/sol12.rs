d1d343df078334622f74b535ef84d09fdab50086
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-12-18 20:44:34 +0300

    Refactor day 12: CaveType enum, don't construct path explicitly

--- src/sol12.rs
+++ src/sol12.rs
@@ -1,3 +1,11 @@
+#[derive(Clone, Copy, PartialEq)]
+enum CaveType {
+    Terminal,  // start or end
+    Small,
+    Big,
+}
+use CaveType::*;
+
 pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
     let mut names: Vec<&str> = vec![];
     let mut adj: Vec<Vec<usize>> = vec![];
@@ -23,41 +31,44 @@ pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
 
     let start = find_name("start", &mut names, &mut adj);
     let end = find_name("end", &mut names, &mut adj);
-    let is_small: Vec<bool> = names.iter()
-        .map(|name| name.chars().next().unwrap().is_ascii_lowercase())
-        .collect();
+    let cave_type: Vec<CaveType> = names.iter()
+        .map(|&name|
+            if name == "start" || name == "end" {
+                Terminal
+            } else if name.chars().next().unwrap().is_ascii_lowercase() {
+                Small
+            } else {
+                Big
+            }
+        ).collect();
     let mut visited = vec![false; names.len()];
 
     visited[start] = true;
-    let mut path = vec![start];
     let mut num_paths = 0;
 
-    rec(&is_small, &adj, end, &mut path, &mut visited, &mut num_paths);
+    rec(&cave_type, &adj, end, start, &mut visited, &mut num_paths);
     out(num_paths.to_string());
 }
 
 fn rec(
-    is_small: &[bool], adj: &[Vec<usize>], end: usize,
-    path: &mut Vec<usize>, visited: &mut Vec<bool>, num_paths: &mut usize,
+    cave_type: &[CaveType], adj: &[Vec<usize>], end: usize,
+    last: usize,
+    visited: &mut Vec<bool>, num_paths: &mut usize,
 ) {
-    let last = *path.last().unwrap();
     if last == end {
         *num_paths += 1;
     }
     for &u in &adj[last] {
-        if is_small[u] {
+        if cave_type[u] != Big {
             if visited[u] {
                 continue;
             }
             visited[u] = true;
         }
-        path.push(u);
 
-        rec(is_small, adj, end, path, visited, num_paths);
+        rec(cave_type, adj, end, u, visited, num_paths);
 
-        let u2 = path.pop().unwrap();
-        assert_eq!(u, u2);
-        if is_small[u] {
+        if cave_type[u] != Big {
             visited[u] = false;
         }
     }
