2cbb12b52fb931bf52eb5deb88ad4cb2d10576f8
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-12-18 23:30:45 +0300

    Day 14, part 2

--- src/sol14.rs
+++ src/sol14.rs
@@ -11,12 +11,12 @@ pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
         .collect();
 
     const N: usize = b'Z' as usize + 1;
-    let mut pair_counts = vec![0; N * N];
+    let mut pair_counts = vec![0i64; N * N];
     for (&a, &b) in template.iter().zip(&template[1..]) {
         pair_counts[a as usize * N + b as usize] += 1;
     }
 
-    for _ in 0..10 {
+    for step in 0..40 {
         let mut new_pair_counts = pair_counts.clone();
         for &((p1, p2), ins) in &rules {
             let k = pair_counts[p1 as usize * N + p2 as usize];
@@ -25,21 +25,24 @@ pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
             new_pair_counts[ins as usize * N + p2 as usize] += k;
         }
         pair_counts = new_pair_counts;
-    }
-    let mut char_counts = vec![0; N];
-    for p1 in 0..N {
-        for p2 in 0..N {
-            char_counts[p1] += pair_counts[p1 * N + p2];
-            char_counts[p2] += pair_counts[p1 * N + p2];
+
+        if step + 1 == 10 || step + 1 == 40 {
+            let mut char_counts = vec![0; N];
+            for p1 in 0..N {
+                for p2 in 0..N {
+                    char_counts[p1] += pair_counts[p1 * N + p2];
+                    char_counts[p2] += pair_counts[p1 * N + p2];
+                }
+            }
+            char_counts[*template.first().unwrap() as usize] += 1;
+            char_counts[*template.last().unwrap() as usize] += 1;
+            for k in &mut char_counts {
+                assert_eq!(*k % 2, 0);
+                *k /= 2;
+            }
+            let max = *char_counts.iter().max().unwrap();
+            let min = *char_counts.iter().filter(|&&k| k > 0).min().unwrap();
+            out((max - min).to_string());
         }
     }
-    char_counts[*template.first().unwrap() as usize] += 1;
-    char_counts[*template.last().unwrap() as usize] += 1;
-    for k in &mut char_counts {
-        assert_eq!(*k % 2, 0);
-        *k /= 2;
-    }
-    let max = *char_counts.iter().max().unwrap();
-    let min = *char_counts.iter().filter(|&&k| k > 0).min().unwrap();
-    out((max - min).to_string());
 }
