7733bdd92cc05ea67f52c3da053c49d09177b514
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-12-19 19:19:11 +0300

    Day 17, part 2

--- src/sol17.rs
+++ src/sol17.rs
@@ -5,8 +5,9 @@ pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
     let x_range = parse_range(x_range);
     let y_range = parse_range(y_range);
     let mut part1 = 0;
+    let mut part2 = 0;
     for vx0 in 1 ..= *x_range.end() {
-        for vy0 in 1 ..= -*y_range.start() {
+        for vy0 in *y_range.start() ..= -*y_range.start() {
             let mut vx = vx0;
             let mut vy = vy0;
             let mut x = 0;
@@ -16,6 +17,7 @@ pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
                 max_y = max_y.max(y);
                 if x_range.contains(&x) && y_range.contains(&y) {
                     part1 = part1.max(max_y);
+                    part2 += 1;
                     break;
                 }
                 if x > *x_range.end() || y < *y_range.start() {
@@ -29,6 +31,7 @@ pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
         }
     }
     out(part1.to_string());
+    out(part2.to_string());
 }
 
 fn parse_range(r: &str) -> std::ops::RangeInclusive<i32> {
