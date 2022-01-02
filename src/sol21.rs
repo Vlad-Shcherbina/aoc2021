96c7fae13a5dcd7605d4b4746c0795a071a8e220
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2022-01-02 16:24:30 +0300

    Day 21, part 2

--- src/sol21.rs
+++ src/sol21.rs
@@ -1,14 +1,15 @@
-pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
-    let mut scores = [0, 0];
-    let mut positions = [0, 0];
+use fxhash::FxHashMap as HashMap;
 
+pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
     let mut it = input.split_terminator('\n');
     let line = it.next().unwrap();
-    positions[0] = line.strip_prefix("Player 1 starting position: ").unwrap().parse().unwrap();
+    let start1: i32 = line.strip_prefix("Player 1 starting position: ").unwrap().parse().unwrap();
     let line = it.next().unwrap();
-    positions[1] = line.strip_prefix("Player 2 starting position: ").unwrap().parse().unwrap();
+    let start2: i32 = line.strip_prefix("Player 2 starting position: ").unwrap().parse().unwrap();
     assert!(it.next().is_none());
 
+    let mut scores = [0, 0];
+    let mut positions = [start1, start2];
     let mut num_dice_rolls = 0;
     let mut dice = 1;
     for t in 0.. {
@@ -25,4 +26,35 @@ pub(crate) fn solve(input: &str, out: &mut dyn FnMut(String)) {
             break;
         }
     }
+
+    let mut win_cnt = [0, 0];
+    let mut cnts = HashMap::default();
+    cnts.insert(([0, 0], [start1, start2]), 1u64);
+    for t in 0.. {
+        if cnts.is_empty() {
+            break;
+        }
+        let player = t % 2;
+        let mut new_cnts = HashMap::default();
+        for ((scores, poss), cnt) in cnts {
+            for d1 in 1..4 {
+                for d2 in 1..4 {
+                    for d3 in 1..4 {
+                        let mut new_poss = poss;
+                        new_poss[player] += d1 + d2 + d3;
+                        new_poss[player] = (new_poss[player] + 10 - 1) % 10 + 1;
+                        let mut new_scores = scores;
+                        new_scores[player] += new_poss[player];
+                        if new_scores[player] >= 21 {
+                            win_cnt[player] += cnt;
+                        } else {
+                            *new_cnts.entry((new_scores, new_poss)).or_default() += cnt;
+                        }
+                    }
+                }
+            }
+        }
+        cnts = new_cnts;
+    }
+    out(win_cnt[0].max(win_cnt[1]).to_string());
 }
