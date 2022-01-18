ae24926cd6d80bd70a11d0d9508910b819db1198
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2022-01-18 19:46:58 +0100

    Speed up day 24 (compute possible z ranges backwards)

--- src/sol24.rs
+++ src/sol24.rs
@@ -1,4 +1,5 @@
 use std::fmt::Write;
+use std::ops::Range;
 
 pub(crate) fn solve(mut input: &str, out: &mut dyn FnMut(String)) {
     let mut blocks = vec![];
@@ -8,11 +9,19 @@ pub(crate) fn solve(mut input: &str, out: &mut dyn FnMut(String)) {
         input = rest;
     }
 
+    let mut allowed_ranges = vec![];
+    let mut z = Range { start: 0, end: 1 };
+    for b in blocks.iter().rev() {
+        z = b.run_range_backwards(z);
+        allowed_ranges.push(z.clone());
+    }
+    allowed_ranges.reverse();
+
     let mut digits: Vec<i64> = (1..=9).collect();
     for _ in 0..2 {
         digits.reverse();
         let mut ws = vec![];
-        let res = rec(&digits, &blocks, &mut ws, 0);
+        let res = rec(&digits, &blocks, &allowed_ranges, &mut ws, 0);
         assert!(res);
 
         let mut s = String::new();
@@ -24,25 +33,24 @@ pub(crate) fn solve(mut input: &str, out: &mut dyn FnMut(String)) {
 
 }
 
-fn rec(digits: &[i64], blocks: &[Block], ws: &mut Vec<i64>, z: i64) -> bool {
+fn rec(
+    digits: &[i64],
+    blocks: &[Block],
+    allowed_ranges: &[Range<i64>],
+    ws: &mut Vec<i64>,
+    z: i64,
+) -> bool {
     match blocks.split_first() {
         None => z == 0,
         Some((block, blocks)) => {
-            let mut tz = z;
-            for b in blocks {
-                match b.dz {
-                    1 => {}
-                    26 => tz /= 26,
-                    _ => panic!(),
-                }
-            }
-            if tz.abs() > 30 {
+            let (rz, allowed_ranges) = allowed_ranges.split_first().unwrap();
+            if !rz.contains(&z) {
                 return false;
             }
             for &w in digits {
                 let z1 = block.run(z, w);
                 ws.push(w);
-                if rec(digits, blocks, ws, z1) {
+                if rec(digits, blocks, allowed_ranges, ws, z1) {
                     return true;
                 }
                 ws.pop().unwrap();
@@ -60,6 +68,24 @@ struct Block {
 }
 
 impl Block {
+    fn run_range_backwards(&self, z: Range<i64>) -> Range<i64> {
+        let start = (z.start - self.ay).max(0);
+        let z1 = Range { start, end: (z.end - self.ay).max(start) };
+        let z1 = backwards_range_mul(z1, 26);
+        let z1 = Range {
+            start: z1.start.min(z.start),
+            end: z1.end.max(z.end),
+        };
+        assert!(0 <= z1.start);
+        assert!(z1.start <= z1.end);
+
+        match self.dz {
+            1 => z1,
+            26 => backwards_range_div(z1, 26),
+            _ => panic!()
+        }
+    }
+
     fn run(&self, mut z: i64, w: i64) -> i64 {
         let x = z % 26 + self.ax;
         match self.dz {
@@ -99,6 +125,7 @@ add y w
 add y ").unwrap();
         let (ay, input) = input.split_once('\n').unwrap();
         let ay: i64 = ay.parse().unwrap();
+        assert!(ay >= 0);
         let input = input.strip_prefix("mul y x\nadd z y\n").unwrap();
         (Block { dz, ax, ay}, input)
     }
@@ -123,3 +150,54 @@ add y <ay>
 mul y x
 add z y
 */
+
+fn backwards_range_div(r: Range<i64>, d: i64) -> Range<i64> {
+    assert!(r.start >= 0);
+    Range { start: r.start * d, end: r.end * d }
+}
+
+#[test]
+fn test_backwards_range_div() {
+    for x1 in 0..10 {
+        for x2 in x1..10 {
+            let r = Range { start: x1, end: x2 };
+            for d in 1..3 {
+                let pr = backwards_range_div(r.clone(), d);
+                if pr.start > 0 {
+                    assert!(!r.contains(&((pr.start - 1) / d)));
+                }
+                assert!(!r.contains(&(pr.end / d)));
+                for px in pr {
+                    assert!(r.contains(&(px / d)));
+                }
+            }
+        }
+    }
+}
+
+fn backwards_range_mul(r: Range<i64>, m: i64) -> Range<i64> {
+    assert!(r.start >= 0);
+    Range {
+        start: (r.start + m - 1) / m,
+        end: (r.end + m - 1) / m,
+    }
+}
+
+#[test]
+fn test_backwards_range_mul() {
+    for x1 in 0..10 {
+        for x2 in x1..10 {
+            let r = Range { start: x1, end: x2 };
+            for m in 1..3 {
+                let pr = backwards_range_mul(r.clone(), m);
+                if pr.start > 0 {
+                    assert!(!r.contains(&((pr.start - 1) * m)));
+                }
+                assert!(!r.contains(&(pr.end * m)));
+                for px in pr {
+                    assert!(r.contains(&(px * m)));
+                }
+            }
+        }
+    }
+}
